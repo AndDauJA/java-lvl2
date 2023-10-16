0464fe6a7eb571b57d14544e0dde583052427d03
Andrius <daujotasand@gmail.com>
2023-10-16 20:27:22 +0300

    delete surname changed name to fullname

--- src/lt/manoprogramos/lv2/git/StudnetDto.java
+++ src/lt/manoprogramos/lv2/git/StudnetDto.java
@@ -1,14 +1,11 @@
 package lt.manoprogramos.lv2.git;
 
 public class StudnetDto {
-    public final String name(String surname) {
-        return name("Daujotas");
+    public final String fllName() {
+        return name();
 
     }
-    public final String surname(){
 
-        return surname();
-    }
 
     public StudnetDto() {
     }
