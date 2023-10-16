04cbd0c50f4009fa38c65f4438e6a72ae382c589
Andrius <daujotasand@gmail.com>
2023-10-16 20:25:46 +0300

    Student class add Constructor

--- src/lt/manoprogramos/lv2/git/StudnetDto.java
+++ src/lt/manoprogramos/lv2/git/StudnetDto.java
@@ -1,8 +1,8 @@
 package lt.manoprogramos.lv2.git;
 
 public class StudnetDto {
-    public final String name() {
-        return name();
+    public final String name(String surname) {
+        return name("Daujotas");
 
     }
     public final String surname(){
